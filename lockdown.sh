01750a5a195cf7a080980c84e2336662ec13c9bc
Joe Guilmette <joe.guilmette@gmail.com>
2015-03-21 14:00:15 +0700

    updated wordmove perms

--- lockdown.sh
+++ lockdown.sh
@@ -1,20 +1,10 @@
 #!/bin/bash  
   
 echo "Setting 755 for all folders in /var/www"  
-sudo find /var/www/ -type d -exec chmod 755 {} \; # 755 all folders  
+sudo find /var/www/*/htdocs/ -type d -exec chmod 0775 {} \; # 755 all folders  
 echo "Setting 644 for all files in /var/www"  
-sudo find /var/www/ -type f -exec chmod 644 {} \; # 644 all files.  
+sudo find /var/www/*/htdocs/ -type f -exec chmod 0675 {} \; # 644 all files.
 echo "Setting ownership for /var/www to www-data"  
-sudo chown -R www-data:www-data /var/www/  
-echo "Setting proper permissions for /var/www/.ssh"
-sudo chmod 700 /var/www/.ssh/
-sudo chmod 600 /var/www/.ssh/authorized_keys
-sudo chmod 600 /var/www/.ssh/id_rsa
-sudo chmod 644 /var/www/.ssh/id_rsa.pub
-sudo chown -R www-data:www-data /var/www/.ssh
-
-echo "Current www-data shell settings:"  
-cat /etc/passwd | grep www-data  
-echo "Setting www-data to /sbin/nologin"  
-sudo usermod -s /usr/sbin/nologin www-data  
-cat /etc/passwd | grep www-data 
+sudo chown -R www-data:www-data /var/www/
+echo "Change group for /var/www/*/htdocs to wordmove"  
+sudo chgrp -R wordmove /var/www/*/htdocs/
